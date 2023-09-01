726e7e4e9cd181ca0f52041084311ab209bed157
samuel-wakoli <swwakoli@gmail.com>
2023-09-01 18:11:37 +0300

    added Bottom Nav pages

---
+++ app/src/main/java/com/safe_buddy/safebuddy/ui/pages/PaymentPage.kt
@@ -0,0 +1,32 @@
+package com.safe_buddy.safebuddy.ui.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PaymentPage() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Payment", fontSize = 26.sp)
+    }
+}
+
+@Preview
+@Composable
+fun PaymentPagePreview() {
+    PaymentPage()
+}
