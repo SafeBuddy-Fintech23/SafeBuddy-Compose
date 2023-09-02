f0c06894a3783e46cac06fa37a4c7fefdab9b0c8
samuel-wakoli <swwakoli@gmail.com>
2023-09-02 15:53:36 +0300

    added credit card

--- app/src/main/java/com/safe_buddy/safebuddy/ui/pages/bottom_nav_destinations/HomePage.kt
+++ app/src/main/java/com/safe_buddy/safebuddy/ui/pages/bottom_nav_destinations/HomePage.kt
@@ -1,14 +1,22 @@
 package com.safe_buddy.safebuddy.ui.pages.bottom_nav_destinations
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,9 +26,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.safe_buddy.safebuddy.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,6 +48,7 @@ fun HomePage() {
         Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         DockedSearchBar(
             modifier = Modifier
@@ -56,6 +73,7 @@ fun HomePage() {
                 // TODO: Add content here
             }
         }
+        CustomVisaCard()
 
     }
 }
@@ -64,4 +82,87 @@ fun HomePage() {
 @Composable
 fun HomePagePreview() {
     HomePage()
+}
+
+@Composable
+fun CustomVisaCard(
+) {
+    Card(
+        modifier = Modifier
+            .width(400.dp)
+            .height(260.dp)
+            .padding(8.dp)
+    ) {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.img_card_background),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Column(Modifier.padding(8.dp)) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_round),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.size(80.dp)
+                    )
+                    Text(
+                        text = "Credit Card",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        fontFamily = FontFamily.Monospace
+                    )
+                }
+                Text(
+                    text = "3646 5926 5358 6346",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = "Balance",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace
+                )
+                Text(
+                    text = "$ 5,000.00",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Monospace
+                )
+                Text(
+                    text = "username username",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Monospace,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = "SafeBuddy",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = FontFamily.Monospace,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CustomVisaCardPreview() {
+    CustomVisaCard()
 }
